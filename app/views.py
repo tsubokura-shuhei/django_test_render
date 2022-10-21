dcf2be899959fee460dc539fa19a039b5e628d03
tsubokura <repeat2639@gmail.com>
2022-10-22 00:19:07 +0900

    first commit

---
+++ app/views.py
@@ -0,0 +1,6 @@
+from django.views.generic import View
+from django.shortcuts import render
+
+class IndexView(View):
+  def get(self, request, *args, **kwargs):
+    return render(request, "app/index.html")
